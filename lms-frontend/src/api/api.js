107c3b452b5ab29afb359fa2c3a16df8845f6ec5
KuI3uS <kuba.marcinkowski98@wp.pl>
2026-05-17 12:23:02 +0200

    Update API URL

--- lms-frontend/src/api/api.js
+++ lms-frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/api";
+const API_URL = "https://lms-platform-1-dcxg.onrender.com/api";
 
 export function getToken() {
     return localStorage.getItem("token");
