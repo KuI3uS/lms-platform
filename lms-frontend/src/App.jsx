f6dafa638931902fb1ca3a5f45c3e66c06316a3c
KuI3uS <kuba.marcinkowski98@wp.pl>
2026-05-19 16:48:56 +0200

    fix courses route and landing route

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -36,6 +36,7 @@ export default function App() {
         <Routes>
 
             {/* ===== PUBLIC ===== */}
+            <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
