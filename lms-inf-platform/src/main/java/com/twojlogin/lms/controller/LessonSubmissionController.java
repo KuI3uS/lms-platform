82fd47dd02e79cbc6f25c21e297f78d06b065e32
KuI3uS <kuba.marcinkowski98@wp.pl>
2026-06-23 11:07:58 +0200

    fix delate

--- lms-inf-platform/src/main/java/com/twojlogin/lms/controller/LessonSubmissionController.java
+++ lms-inf-platform/src/main/java/com/twojlogin/lms/controller/LessonSubmissionController.java
@@ -7,6 +7,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.Authentication;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 import java.util.List;
 
 @RestController
